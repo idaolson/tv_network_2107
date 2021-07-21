84a3bb1e17a35a0ea2c3a7337c17b13ca2bf7318
Ida Olson <idaolson@gmail.com>
2021-07-21 09:43:36 -0600

    Complete iteration 2

--- lib/show.rb
+++ lib/show.rb
@@ -1,1 +1,26 @@
+require './lib/character'
 
+class Show
+attr_reader :name, :creator, :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    all_salaries = characters.map { |character| character.salary }.sum
+  end
+
+  def highest_paid_actor
+    characters.max_by do |character|
+      character.salary
+      return character.actor
+    end
+  end
+
+  def actors
+    all_actors = characters.map { |character| character.actor }
+  end
+end
